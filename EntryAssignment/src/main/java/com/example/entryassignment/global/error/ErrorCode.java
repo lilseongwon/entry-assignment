42edd50442a0caee26ebbdc8b529cd99336e79ab
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-11 15:04:03 +0900

    📝 :: ErrorCode

---
+++ EntryAssignment/src/main/java/com/example/entryassignment/global/error/ErrorCode.java
@@ -0,0 +1,40 @@
+package com.example.entryassignment.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    PASSWORD_MISMATCH(401, "Password Mismatch"),
+
+    EXPIRED_JWT(401, "Expired Jwt"),
+    SIGNATURE_JWT(401, "Signature Jwt"),
+    INVALID_JWT(401, "Invalid Jwt"),
+
+    NO_PERMISSION_TO_ADD_BOOK(401, "No Permission To Add Book"),
+    NO_PERMISSION_TO_MODIFY_BOOK(401, "No Permission To Modify Book"),
+    NO_PERMISSION_TO_DELETE_BOOK(401, "No Permission To Delete Book"),
+    NO_PERMISSION_TO_CHECK_APPLY_LIST(401, "No Permission To Check Apply List"),
+    NO_PERMISSION_TO_CHECK_USER_INFO(401, "No Permission To Check User Info"),
+
+    INVALID_REFRESH_TOKEN(401, "Invalid Refresh Token"),
+
+    AUTH_NOT_FOUND(404, "Auth Not Found"),
+
+    BOOK_IS_NOT_EXIST(404, "Book is Not Exist"),
+    BOOK_NOT_FOUND(404, "Book Not Found"),
+
+    REFRESH_TOKEN_NOT_FOUND(404, "RefreshToken Not Found"),
+
+    EMAIL_EXIST(409, "Email Exist"),
+
+    ALREADY_BOOK_EXIST(409, "Already Book Exist"),
+    ALREADY_BOOK_IS_ON_LOAN(409, "Already Book is On Loan"),
+    ALREADY_APPLIED(409, "Already Applied"),
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int status;
+    private final String message;
+}
