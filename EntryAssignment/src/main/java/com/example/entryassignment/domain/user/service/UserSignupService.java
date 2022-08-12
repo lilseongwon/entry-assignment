24056aa6e0d1c597280859ef26d0e45f79ddeca5
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-12 20:12:35 +0900

    ♻️ :: 매개변수(dto) 이름 변경

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/user/service/UserSignupService.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/user/service/UserSignupService.java
@@ -3,7 +3,7 @@ package com.example.entryassignment.domain.user.service;
 import com.example.entryassignment.domain.user.domain.User;
 import com.example.entryassignment.domain.user.domain.repository.UserRepository;
 import com.example.entryassignment.domain.user.exception.UserExistException;
-import com.example.entryassignment.domain.user.presentation.dto.request.UserSignupRequestDto;
+import com.example.entryassignment.domain.user.presentation.dto.request.UserSignupRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,16 +16,16 @@ public class UserSignupService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public String execute(UserSignupRequestDto requestDto) {
-        Optional<User> user = userRepository.findByAccountId(requestDto.getAccountId());
+    public String execute(UserSignupRequest request) {
+        Optional<User> user = userRepository.findByAccountId(request.getAccountId());
         if (user.isPresent())
             throw UserExistException.EXCEPTION;
 
         userRepository.save(
                 User.builder()
-                        .accountId(requestDto.getAccountId())
-                        .password(passwordEncoder.encode(requestDto.getPassword()))
-                        .name(requestDto.getName())
+                        .accountId(request.getAccountId())
+                        .password(passwordEncoder.encode(request.getPassword()))
+                        .name(request.getName())
                         .build());
         return "회원가입이 완료되었습니다.";
     }
