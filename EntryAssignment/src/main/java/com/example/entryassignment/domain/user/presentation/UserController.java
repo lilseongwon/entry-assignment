9ad9a874634aa57879dca44b364e0a7ac4afa2c4
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-12 19:32:26 +0900

    ♻️ :: 메소드명 수정

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/user/presentation/UserController.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/user/presentation/UserController.java
@@ -1,6 +1,6 @@
 package com.example.entryassignment.domain.user.presentation;
 
-import com.example.entryassignment.domain.user.presentation.dto.request.SignupRequestDto;
+import com.example.entryassignment.domain.user.presentation.dto.request.UserSignupRequestDto;
 import com.example.entryassignment.domain.user.service.UserSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,8 @@ public class UserController {
     private final UserSignupService userSignupService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
+    public String userSignup(@RequestBody @Valid UserSignupRequestDto signupRequestDto) {
         return userSignupService.execute(signupRequestDto);
     }
+}
 
