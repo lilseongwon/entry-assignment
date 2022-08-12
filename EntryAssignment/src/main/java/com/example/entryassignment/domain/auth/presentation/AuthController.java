e6eee956118b5efa7c99669d8c03d7cea0b21278
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-12 23:43:22 +0900

    ♻️ :: RequestMapping "/user"로 수정

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/auth/presentation/AuthController.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/auth/presentation/AuthController.java
@@ -1,6 +1,6 @@
 package com.example.entryassignment.domain.auth.presentation;
 
-import com.example.entryassignment.domain.auth.presentation.dto.request.SignInRequestDto;
+import com.example.entryassignment.domain.auth.presentation.dto.request.SignInRequest;
 import com.example.entryassignment.domain.auth.presentation.dto.response.TokenResponse;
 import com.example.entryassignment.domain.auth.service.TokenRefreshService;
 import com.example.entryassignment.domain.auth.service.UserSignInService;
@@ -11,14 +11,14 @@ import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/user")
 public class AuthController {
     private final UserSignInService userSignInService;
     private final TokenRefreshService tokenRefreshService;
 
     @PostMapping("/token")
-    public TokenResponse userSignIn(@RequestBody @Valid SignInRequestDto signInRequestDto) {
-        return userSignInService.execute(signInRequestDto);
+    public TokenResponse userSignIn(@RequestBody @Valid SignInRequest signInRequest) {
+        return userSignInService.execute(signInRequest);
     }
     @PatchMapping
     public TokenResponse Reisssu(@RequestHeader("Refresh-Token") String refreshToken){
