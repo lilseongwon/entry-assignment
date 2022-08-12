c7ef7871a52b1f795d42530a6a1956abb358ce0f
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-13 03:21:30 +0900

    🔥 :: 안쓰는 import 삭제

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/request/AdminAddBookRequest.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/request/AdminAddBookRequest.java
@@ -4,8 +4,6 @@ import com.example.entryassignment.domain.book.domain.Genre;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
