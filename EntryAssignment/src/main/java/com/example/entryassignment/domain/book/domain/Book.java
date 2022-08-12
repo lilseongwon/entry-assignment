a07f3b4b99de961bb9dc84afb67002314a04b9be
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-13 02:41:40 +0900

    🐛 :: @Entity 추가
    진짜 쓰러지기 직전

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/book/domain/Book.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/domain/Book.java
@@ -5,13 +5,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
