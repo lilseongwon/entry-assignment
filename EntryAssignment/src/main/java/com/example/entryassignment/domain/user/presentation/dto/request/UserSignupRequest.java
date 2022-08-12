fe07fc21212507539c81684386a6e60c979b581b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-12 23:40:58 +0900

    ♻️ :: 검증 메세지 구조 수정

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/user/presentation/dto/request/UserSignupRequest.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/user/presentation/dto/request/UserSignupRequest.java
@@ -8,19 +8,19 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor
-@NotBlank
 public class UserSignupRequest {
 
-    @NotBlank(message = "아이디는 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "^[a-z0-9-_]{8,20}$", message = "아이디는 영어와 숫자를 조합한 8~20자리여야 합니다.")
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z]{1}[a-zA-Z0-9_]{8,20}$",
+            message = "아이디는 공백과 띄어쓰기를 허용하지 않고, 영어와 숫자를 조합한 8~20자리여야 합니다.")
     private String accountId;
 
-    @NotBlank(message = "비밀번호는 공백, 띄어쓰기를 허용하지 않습니다.")
+    @NotBlank
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,60}",
-            message = "비밀번호는 영어, 숫자, 특수기호를 포함한 8~60자리여야 합니다.")
+            message = "비밀번호는 공백과 띄어쓰기를 허용하지 않고, 영어, 숫자, 특수기호를 포함한 8~60자리여야 합니다.")
     private String password;
 
-    @NotBlank(message = "이름은 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "^[가-힣]{2,4}$", message = "이름은 한글을 사용하고, 2~4자리여야 합니다.")
+    @NotBlank
+    @Pattern(regexp = "^[가-힣]{2,4}$", message = "이름은 공백과 띄어쓰기를 허용하지 않고, 한글만을 포함한 2~4자리여야 합니다.")
     private String name;
 }
