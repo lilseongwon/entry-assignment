d2ccc128dd0ead9ad14c4056cac32a6ca29d0702
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-13 23:39:02 +0900

    ♻️ :: 어노테이션 순서 수정

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/response/QueryBookInfoListResponse.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/response/QueryBookInfoListResponse.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public class QueryBookInfoListResponse {
     private final List<QueryBookInfoResponse> bookLists;
 }
