ea77a4033003a8c6338a69480228c87f6a662f49
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-13 15:35:32 +0900

    📝 :: BookFacade
    도서 중복확인, 도서검색 메소드 추가

---
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/facade/BookFacade.java
@@ -0,0 +1,25 @@
+package com.example.entryassignment.domain.book.facade;
+
+import com.example.entryassignment.domain.book.domain.Book;
+import com.example.entryassignment.domain.book.domain.repository.BookRepository;
+import com.example.entryassignment.domain.book.exception.AlreadyBookExistExcpetion;
+import com.example.entryassignment.domain.book.exception.BookIsNotExistException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BookFacade {
+
+    private final BookRepository bookRepository;
+
+    public void checkBookExist(String title, String isbn){
+        if(bookRepository.findByTitleAndIsbn(title, isbn).isPresent())
+            throw AlreadyBookExistExcpetion.EXCPETION;
+    }
+
+    public Book getBookByTitleAndIsbn(String title, String isbn){
+        return bookRepository.findByTitleAndIsbn(title, isbn)
+                .orElseThrow(() -> BookIsNotExistException.EXCEPTION);
+    }
+}
