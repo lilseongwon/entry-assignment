2e13a835ddfc1d3c09bedc5875d770e1f323e2e1
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-13 21:48:12 +0900

    🔥 :: 잘못된 에러코드 삭제

--- EntryAssignment/src/main/java/com/example/entryassignment/global/error/ErrorCode.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/global/error/ErrorCode.java
@@ -19,14 +19,12 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "User Not Found"),
 
     BOOK_IS_NOT_EXIST(404, "Book is Not Exist"),
-    BOOK_NOT_FOUND(404, "Book Not Found"),
 
     REFRESH_TOKEN_NOT_FOUND(404, "RefreshToken Not Found"),
 
     USER_EXIST(409, "User Exist"),
 
     ALREADY_BOOK_EXIST(409, "Already Book Exist"),
-    ALREADY_BOOK_IS_ON_LOAN(409, "Already Book is On Loan"),
     ALREADY_APPLIED(409, "Already Applied"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
