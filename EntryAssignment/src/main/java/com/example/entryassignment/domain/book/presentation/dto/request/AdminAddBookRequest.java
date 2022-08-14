d34ed67627b31a4f9cc4013d287e2efd3db75aac
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-14 22:01:43 +0900

    🐛 :: Enum 제약조건 삭제
    시간이 없어서 검증은 담에;;

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/request/AdminAddBookRequest.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/request/AdminAddBookRequest.java
@@ -4,7 +4,6 @@ import com.example.entryassignment.domain.book.domain.Genre;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -14,18 +13,17 @@ public class AdminAddBookRequest {
     @Size(min = 1, max = 60, message = "책 제목은 Null을 허용하지 않으며, 1~60자 내외여야 합니다.")
     private String title;
 
+    @Size(min = 1, max = 300, message = "사진은 Null을 허용하지 않으며, 1~300자 내외여야 합니다.")
     private String picture;
 
     @Size(min = 1, max = 20, message = "저자는 Null을 허용하지 않으며, 1~20자 내외여야 합니다.")
     private String author;
 
-    @NotBlank
-    @Size(min = 10, max = 20, message = "ISBN은 Null과 공백을 허용하지 않으며, 10~20자 이하여야 합니다.")
-    private Integer isbn;
+    @Size(min = 1, max = 20, message = "ISBN은 Null을 허용하지 않으며, 10~20자 내외여야 합니다.")
+    private String isbn;
 
     @Size(min = 1, max = 60, message = "출판사는 Null을 허용하지 않으며, 1~60자 내외여야 합니다.")
     private String publisher;
 
-    @Size(min = 1, max = 60, message = "장르는 Null을 허용하지 않으며, enum클래스에 있는 옵션을 이용한 1~10자 내외여야 합니다.")
     private Genre genre;
 }
--- EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/request/AdminUpdateBookRequest.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/presentation/dto/request/AdminUpdateBookRequest.java
@@ -4,26 +4,23 @@ import com.example.entryassignment.domain.book.domain.Genre;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class AdminUpdateBookRequest {
 
-    @Size(max = 200, message = "사진 링크는 200자 이하여야 합니다.")
+    @Size(min = 1, max = 300, message = "사진은 Null을 허용하지 않으며, 1~300자 내외여야 합니다.")
     private String picture;
 
     @Size(min = 1, max = 20, message = "저자는 Null을 허용하지 않으며, 1~20자 내외여야 합니다.")
     private String author;
 
-    @NotBlank
-    @Size(min = 10, max = 20, message = "ISBN은 Null과 공백을 허용하지 않으며, 10~20자 이하여야 합니다.")
-    private Integer isbn;
+    @Size(min = 1, max = 20, message = "ISBN은 Null을 허용하지 않으며, 10~20자 내외여야 합니다.")
+    private String isbn;
 
     @Size(min = 1, max = 60, message = "출판사는 Null을 허용하지 않으며, 1~60자 내외여야 합니다.")
     private String publisher;
 
-    @Size(min = 1, max = 60, message = "장르는 Null을 허용하지 않으며, enum클래스에 있는 옵션을 이용한 1~10자 내외여야 합니다.")
     private Genre genre;
 }
